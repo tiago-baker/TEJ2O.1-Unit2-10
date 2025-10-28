0c64a4e29c11f152249991bfeff403425c0f0c6e
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-28 18:01:45 -0400

    if stantments fixed

--- main.ts
+++ main.ts
@@ -9,32 +9,46 @@ let lightLevel: number = 0
 let strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
 
 basic.forever(function () {
-    lightLevel = input.lightLevel()
     strip.clear()
+    lightLevel = input.lightLevel()
+    
 
     if (lightLevel <= 51) {
+        strip = neopixel.create(DigitalPin.P0, 0, NeoPixelMode.RGB)
+        strip.setBrightness(255)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
     }
 
     if (lightLevel > 52) {
+        strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+        strip.setBrightness(255)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
+
     }
 
     if (lightLevel > 104) {
+        strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+        strip.setBrightness(255)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
+
     }
 
     if (lightLevel > 156) {
+        strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+        strip.setBrightness(255)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
+
     }
 
     if (lightLevel > 208) {
+        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip.setBrightness(255)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         strip.show()
+
     }
-    basic.pause(1000)
 })
